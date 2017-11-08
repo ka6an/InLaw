23f7709279b9607041c662b3a4a7bbba9d41c509
Aleksey Kabanets <kabanets.alexey@mail.ru>
2017-11-09 01:06:01 +0300

    add index.html and about.html

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
-    $('#menu').on('click', function() {
-        $(this).toggleClass('active');
+    $('.header__link').on('mouseenter', function() { // навели курсор
+        $(this).addClass('hovered');
+        if ($(this).data('name') === "servises") // навели на Услуги
+            $('#header-serises-subMenu').addClass('open');
+    });
+
+    var fun = {
+        test: function() {
+            $(this).addClass('hovered');
+            $('#header-serises-subMenu').addClass('open');
+        }
+    };
+    $('.header__link').on('mouseleave', function() { // убрали курсор
+        $(this).removeClass('hovered');
+
+        $('#header-serises-subMenu').removeClass('open');
+        console.log(this);
+        $('#header-serises-subMenu').on('mouseenter', $.proxy( fun.test, this) ); // перешли в выпадающий список
+
+        $('#header-serises-subMenu').on('mouseleave', function() { // покинули выпадающий список и пункт меню
+            $('#header-serises-subMenu').removeClass('open');
+            $('#header-servises-link').removeClass('hovered');
+        });
     });
 });
